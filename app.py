6dc4919faa2e53ac574c28d3c4eaa4709450da90
João Pedro <pedrobarros2010@gmail.com>
2021-03-22 18:47:42 -0300

    fix: changing way that video id is passed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from resources.YoutubeExtraction import YoutubeExtraction
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(YoutubeExtraction, '/<string:YtVideoId>')
+api.add_resource(YoutubeExtraction, '/')
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=8080)
