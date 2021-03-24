6c19f48302f9afb940495d7888d922f37b65d059
João Pedro <pedrobarros2010@gmail.com>
2021-03-24 18:16:41 -0300

    refactor: add indentation

--- server/app.py
+++ server/app.py
@@ -9,4 +9,4 @@ api = Api(app)
 api.add_resource(YoutubeExtraction, '/')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
