3352ce36422ff49ab3c72af8cb368d5003eed08c
João Pedro <pedrobarros2010@gmail.com>
2021-04-04 18:54:41 -0300

    feat: parameter remove spoken once words

--- server/resources/functions/countByWordsTimesSpoken.py
+++ server/resources/functions/countByWordsTimesSpoken.py
@@ -3,20 +3,33 @@ from typing import List, Tuple
 ListOfWords = List[str]
 ListOfWordsCounted = List[Tuple[str, int]]
 
-
-def countByWordsTimesSpoken(wordsArr: ListOfWords) -> ListOfWordsCounted:
+def countByWordsTimesSpoken(wordsArr: ListOfWords, removeSpokenJustOnce=False) -> ListOfWordsCounted:
     wordWithCountTuples = []
 
-    for word in wordsArr:
-        # This part adds the word with count in the array just one time
-        flag = 0
-        for tupleWordWithCount in wordWithCountTuples:
-            if tupleWordWithCount[0] == word:
-                flag = 1
-                break
-        if flag == 0:
-            wordOcurruence = wordsArr.count(word)
-            wordWithCountTuples.append((word, wordOcurruence))
+    if removeSpokenJustOnce:
+        for word in wordsArr:
+            # This part adds the word with count in the array just one time
+            flag = 0
+            for tupleWordWithCount in wordWithCountTuples:
+                if tupleWordWithCount[0] == word:
+                    flag = 1
+                    break
+            if flag == 0:
+                wordOcurruence = wordsArr.count(word)
+                if wordOcurruence == 1:
+                    continue
+                wordWithCountTuples.append((word, wordOcurruence))
+    else:
+        for word in wordsArr:
+            # This part adds the word with count in the array just one time
+            flag = 0
+            for tupleWordWithCount in wordWithCountTuples:
+                if tupleWordWithCount[0] == word:
+                    flag = 1
+                    break
+            if flag == 0:
+                wordOcurruence = wordsArr.count(word)
+                wordWithCountTuples.append((word, wordOcurruence))
 
     def wordWithCountTuplesSort(tupleWordWithCount):
         return tupleWordWithCount[1]
