fc6422e3725414c6e5f28e0af1ee0efdfccd6f99
João Pedro <pedrobarros2010@gmail.com>
2021-04-04 22:22:49 -0300

    feat: add routes and global style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './routes'
+import GlobalStyle from './assets/styles/'
 
 function App() {
-	return <div>Hello</div>
+	return (
+		<BrowserRouter>
+			<GlobalStyle />
+			<Routes />
+		</BrowserRouter>
+	)
 }
 
 export default App
