5fc7109c3f291fb85718d2c86711f4d06de016c1
João Pedro <pedrobarros2010@gmail.com>
2021-04-07 13:58:55 -0300

    fix: allowing every origin in cors

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from resources.YoutubeExtraction import YoutubeExtraction
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 api.add_resource(YoutubeExtraction, '/')
 
