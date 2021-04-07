e8b97f00162d2d7d1af335041daf770232c66459
João Pedro <pedrobarros2010@gmail.com>
2021-04-07 14:15:21 -0300

    feat: connect to api and styling the table

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -1,13 +1,50 @@
 import React, { useState } from 'react'
 import Header from '../../components/Header'
+import axios from '../../services/axios'
+import { toast } from 'react-toastify'
 
-import { Button, Form, Input, MainContainer } from './styles'
+import {
+	Button,
+	Form,
+	Input,
+	MainContainer,
+	WordsWithTimesSpoken,
+} from './styles'
+
+interface WordsTimesSpoken extends Array<[string, number]> {}
 
 function MainPage() {
 	const [youtubeLink, setYoutubeLink] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
+	const [wordsTimesSpoken, setWordsTimesSpoken] = useState<WordsTimesSpoken>([])
 
 	function handleChangeYoutubeLink(event: React.ChangeEvent<HTMLInputElement>) {
 		setYoutubeLink(event.target.value)
+	}
+
+	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault()
+		if (isLoading) {
+			return
+		}
+		setIsLoading(true)
+
+		try {
+			const response = await axios.get(`/?videourl=${youtubeLink}`)
+
+			const wordsWithTimesSponkes = response.data
+				.wordsTimesSpoken as WordsTimesSpoken
+
+			setWordsTimesSpoken(wordsWithTimesSponkes)
+		} catch (error) {
+			if (error.message) {
+				toast.error(
+					'O seu video pode não conter legendas, tente novamente depois'
+				)
+			}
+		} finally {
+			setIsLoading(false)
+		}
 	}
 
 	return (
@@ -19,14 +56,32 @@ function MainPage() {
 					Com essa aplicação é possível analisar legendas de vídeos do Youtube.
 					Desenvolvida meramente para fins academicos.
 				</p>
-				<Form>
+				<Form onSubmit={handleSubmit}>
 					<Input
 						placeholder='Url do vídeo'
 						value={youtubeLink}
 						onChange={handleChangeYoutubeLink}
 					/>
-					<Button type='submit'>Enviar</Button>
+					<Button type='submit' loading={String(isLoading)}>
+						Enviar
+					</Button>
 				</Form>
+				<WordsWithTimesSpoken>
+					<tbody>
+						<tr>
+							<th>Palavra</th>
+							<th>Quantidade de vezes falada</th>
+						</tr>
+						{wordsTimesSpoken.map((elem, index) => {
+							return (
+								<tr key={index}>
+									<td>{elem[0]}</td>
+									<td>{elem[1]}</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</WordsWithTimesSpoken>
 			</MainContainer>
 		</React.Fragment>
 	)
--- web/src/pages/Main/styles.ts
+++ web/src/pages/Main/styles.ts
@@ -1,4 +1,8 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+interface ButtonProps {
+	loading?: string
+}
 
 export const MainContainer = styled.main`
 	width: 80%;
@@ -31,7 +35,16 @@ export const Input = styled.input`
 	}
 `
 
-export const Button = styled.button`
+const loading = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+export const Button = styled.button<ButtonProps>`
 	width: 20%;
 	padding: 2rem;
 	border: 0;
@@ -40,14 +53,65 @@ export const Button = styled.button`
 	outline: none;
 	margin-left: 1rem;
 	font-size: 1.7rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	color: var(--color-white);
 
 	background-color: var(--color-secondary);
 	:hover {
 		background-color: var(--color-tertiary);
 	}
+
+	${(props) =>
+		props.loading === 'true'
+			? css`
+					::after {
+						content: '';
+						height: 1.7rem;
+						width: 1.7rem;
+						border: 4px solid transparent;
+						border-bottom: 4px solid var(--color-white);
+						border-radius: 100%;
+						position: absolute;
+
+						animation: ${loading} 1s linear infinite;
+					}
+
+					cursor: not-allowed;
+					color: transparent;
+			  `
+			: ''}
 `
 
 export const Form = styled.form`
 	display: flex;
 	justify-content: center;
+`
+
+export const WordsWithTimesSpoken = styled.table`
+	width: 60%;
+	margin: 0 auto;
+	font-size: 2rem;
+	border-collapse: collapse;
+	margin-top: 5rem;
+
+	td {
+		font-size: 1.6rem;
+		width: 50%;
+		text-align: center;
+	}
+
+	tr {
+		border-bottom: 1px solid #c3c3c3;
+	}
+
+	@media (max-width: 900px) {
+		width: 100%;
+
+		td {
+			width: auto;
+		}
+	}
 `
