12e153f36624597036536eb00904078e223d8b4e
João Pedro <pedrobarros2010@gmail.com>
2021-04-09 21:29:59 -0300

    feat: add page about

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import About from '../pages/About'
 import Main from '../pages/Main'
 
 function Routes() {
 	return (
 		<Switch>
-			<Route path='/'>
+			<Route path='/' exact>
 				<Main />
+			</Route>
+			<Route path='/about'>
+				<About />
 			</Route>
 		</Switch>
 	)
