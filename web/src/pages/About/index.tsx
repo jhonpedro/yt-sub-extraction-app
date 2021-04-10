6f18f4cea167c50710fda2861e0f66dd78348ed1
João Pedro <pedrobarros2010@gmail.com>
2021-04-09 21:34:57 -0300

    wip: add new page about

---
+++ web/src/pages/About/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+import { AboutContainer } from './styles'
+
+function AboutPage() {
+	return (
+		<AboutContainer>
+			<Header />
+			<strong>Sobre</strong>
+		</AboutContainer>
+	)
+}
+
+export default AboutPage
